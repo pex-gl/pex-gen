d4277638453daca6e4296e95a076c43f7d0bf140
Marcin Ignac <marcin.ignac@gmail.com>
2014-04-30 22:45:07 +0100

    Init

---
+++ index.js
@@ -0,0 +1,6 @@
+module.exports.Cube = require('./lib/Cube');
+module.exports.Box = require('./lib/Box');
+module.exports.Sphere = require('./lib/Sphere');
+module.exports.LineBuilder = require('./lib/LineBuilder');
+module.exports.Loft = require('./lib/Loft');
+module.exports.IsoSurface = require('./lib/IsoSurface');
