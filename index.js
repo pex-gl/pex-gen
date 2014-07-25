bbb5afe977f9d211e80d808ea2e5769728235c63
Marcin Ignac <marcin.ignac@gmail.com>
2014-07-25 11:33:34 +0100

    Added platonic solids

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+module.exports.Plane = require('./lib/Plane');
 module.exports.Cube = require('./lib/Cube');
 module.exports.Box = require('./lib/Box');
 module.exports.Sphere = require('./lib/Sphere');
-module.exports.Plane = require('./lib/Plane');
+module.exports.Tetrahedron = require('./lib/Tetrahedron');
+module.exports.Octahedron = require('./lib/Octahedron');
+module.exports.Icosahedron = require('./lib/Icosahedron');
+module.exports.Dodecahedron = require('./lib/Dodecahedron');
 module.exports.LineBuilder = require('./lib/LineBuilder');
 module.exports.Loft = require('./lib/Loft');
 module.exports.IsoSurface = require('./lib/IsoSurface');
